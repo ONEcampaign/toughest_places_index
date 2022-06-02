c3b4dc6b5873d2ab3113567824dde7fd98f3888c
Jorge Rivera <jorge.rivera@one.org>
2022-06-02 07:39:54 -0600

    add function to append (not replace) new data
    - Refreshed data
    - Renamed download function 'refresh'
    - Added read_and_append function to common
    - Added a read_and_append step to data refresh

--- index/wfp_inflation.py
+++ index/wfp_inflation.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from index.common import add_short_names
+from index.common import add_short_names, read_and_append
 from index.config import PATHS
 import country_converter as cc
 
@@ -31,21 +31,27 @@ def _read_inflation(country_iso: str) -> pd.DataFrame:
         print(f"Data not available for {country_iso}")
 
 
-def download_inflation_data(iso_codes: list = None) -> None:
+def refresh_inflation_data(iso_codes: list = None) -> None:
 
     # If no list of iso_codes provided, use all (from coco)
     if iso_codes is None:
         iso_codes = cc.CountryConverter().data["ISO3"].unique()
 
-    # Save each country's data to a csv
+    # Save each country's data to a csv. Don't overwrite existing but append instead
     for iso_code in iso_codes:
         _ = (
             _read_inflation(iso_code)
             .astype({"date": "datetime64[ns]"})
             .pipe(add_short_names, id_col="iso_code", target_col="country_name")
+            .pipe(
+                read_and_append,
+                file_path=rf"{PATHS.raw_wfp_inflation}/{iso_code}.csv",
+                date_col="date",
+                idx=["iso_code", "indicator"],
+            )
             .to_csv(rf"{PATHS.raw_wfp_inflation}/{iso_code}.csv", index=False)
         )
 
 
 if __name__ == "__main__":
-    download_inflation_data()
+    refresh_inflation_data()
