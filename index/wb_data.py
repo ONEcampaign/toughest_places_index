7de654a283f514ba8720326366ce5708300e168c
Jorge Rivera <jorge.rivera@one.org>
2022-06-02 12:25:09 -0600

    Wasting and Stunting functions
    Functions/data from WB for stunting and wasting

--- index/wb_data.py
+++ index/wb_data.py
@@ -1,2 +1,24 @@
 import pandas as pd
-from index import config
+from index import config, common
+
+
+def wb_stunting(refresh: bool = False) -> pd.DataFrame:
+    """Get the World Bank data for stunting"""
+    id_: str = "SH.STA.STNT.ZS"
+
+    return common.wb_data(
+        series=id_, series_name="Stunting (%)", years=15, download=refresh
+    ).pipe(common.add_short_names)
+
+
+def wb_wasting(refresh: bool = False) -> pd.DataFrame:
+    """Get the World Bank data for wasting"""
+    id_: str = "SH.STA.WAST.ZS"
+    return common.wb_data(
+        series=id_, series_name="Wasting (%)", years=15, download=refresh
+    ).pipe(common.add_short_names)
+
+
+if __name__ == "__main__":
+    s = wb_stunting()
+    w = wb_wasting()
