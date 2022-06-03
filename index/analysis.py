27cd4409eccb36da52c6188fc846f7c94da6330c
Jorge Rivera <jorge.rivera@one.org>
2022-06-03 14:47:55 -0600

    Update analysis.py

--- index/analysis.py
+++ index/analysis.py
@@ -1,3 +1,10 @@
+""" THIS FILE EXISTS FOR TESTING PURPOSES ONLY
+
+
+------- IT DOES NOT CONTAIN ACTUAL ANALYSIS YET -------
+
+"""
+
 from index.data_io import ids_data, wb_data, wfp_inflation, wfp_food_sec
 from index.data.indicator import Indicator
 from index.data.dimension import Dimension
@@ -25,19 +32,19 @@ debt_ratio = ids_data.get_service_spending_ratio(2022).rename(
     columns={"ratio": "value"}
 )
 
-#reserves
-reserves = wb_data.wb_reserves().pipe(get_latest, by='iso_code')
+# reserves
+reserves = wb_data.wb_reserves().pipe(get_latest, by="iso_code")
 
 hunger_indicator = Indicator(hunger_1, "hunger")
 nutrition_indicator = Indicator(nutrition_1, "nutrition")
-debt_indicator = Indicator(debt_ratio, 'debt_ratio')
-reserves_indicator = Indicator(reserves,'reserves')
+debt_indicator = Indicator(debt_ratio, "debt_ratio")
+reserves_indicator = Indicator(reserves, "reserves")
 
 
 # ----- Dimensions
 
-dimension1 = Dimension(dimension_name='food')
-dimension2 = Dimension(dimension_name='money')
+dimension1 = Dimension(dimension_name="food")
+dimension2 = Dimension(dimension_name="money")
 
 dimension1.add_indicator(hunger_indicator)
 dimension1.add_indicator(nutrition_indicator)
@@ -45,11 +52,11 @@ dimension1.add_indicator(nutrition_indicator)
 dimension2.add_indicator(debt_indicator)
 dimension2.add_indicator(reserves_indicator)
 
-d1 = dimension1.get_data(orient='long')
-d2 = dimension2.get_data(orient='wide')
+d1 = dimension1.get_data(orient="long")
+d2 = dimension2.get_data(orient="wide")
 
 idx = Index()
 idx.add_dimension(dimension1)
 idx.add_dimension(dimension2)
 
-test = idx.get_data(orient='wide')
+test = idx.get_data(orient="wide")
