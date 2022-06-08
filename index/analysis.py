a510efbcaf243cae7c89d960c1c834f9b24c0be1
Jorge Rivera <jorge.rivera@one.org>
2022-06-08 08:38:02 -0600

    Update analysis.py

--- index/analysis.py
+++ index/analysis.py
@@ -4,59 +4,40 @@
 ------- IT DOES NOT CONTAIN ACTUAL ANALYSIS YET -------
 
 """
+from index.data.index import Index
+from index.indicators import (
+    get_insufficient_food,
+    get_inflation,
+    get_economist_index,
+    get_wasting,
+    get_fiscal_reserves,
+    get_service_spending_ratio,
+)
 
-from index.data_io import ids_data, wb_data, wfp_inflation, wfp_food_sec
 from index.data.indicator import Indicator
 from index.data.dimension import Dimension
-from index.data.index import Index
 
-from index.common import get_latest
-
-import pandas as pd
-
-# 1.1 WFP hunger
-hunger_1 = (
-    wfp_food_sec.read_hunger_data()
-    .filter(["iso_code", "date", "value"], axis=1)
-    .pipe(get_latest, by="iso_code")
+# Crete individual indicators
+insufficient_food = Indicator(get_insufficient_food(), "Insufficient Food Consumption")
+inflation = Indicator(get_inflation(), "Headline Inflation")
+economist_index = Indicator(get_economist_index(), "Economist Index")
+wasting = Indicator(get_wasting(), "Wasting")
+fiscal_reserves = Indicator(get_fiscal_reserves(), "Fiscal Reserves minus gold")
+service_spending_ratio = Indicator(
+    get_service_spending_ratio(2022), "Service Spending Ratio"
 )
 
-nutrition_1 = (
-    wfp_food_sec.read_nutrition_data()
-    .loc[lambda d: d.indicator == "wasting"]
-    .reset_index(drop=True)
-)
+# Create the dimensions
+dimension_1 = Dimension(indicators=[insufficient_food, inflation])
+dimension_2 = Dimension(indicators=[economist_index, wasting])
+dimension_3 = Dimension(indicators=[fiscal_reserves, service_spending_ratio])
 
-# debt
-debt_ratio = ids_data.get_service_spending_ratio(2022).rename(
-    columns={"ratio": "value"}
-)
+# to get a dataframe of all th eindicators in a dimension
+df_dimension_1 = dimension_1.get_data(orient='wide')
+df_dimension_1_long= dimension_1.get_data(orient='long')
 
-# reserves
-reserves = wb_data.wb_reserves().pipe(get_latest, by="iso_code")
+# Create the index object
+index = Index(dimensions=[dimension_1, dimension_2, dimension_3])
 
-hunger_indicator = Indicator(hunger_1, "hunger")
-nutrition_indicator = Indicator(nutrition_1, "nutrition")
-debt_indicator = Indicator(debt_ratio, "debt_ratio")
-reserves_indicator = Indicator(reserves, "reserves")
-
-
-# ----- Dimensions
-
-dimension1 = Dimension(dimension_name="food")
-dimension2 = Dimension(dimension_name="money")
-
-dimension1.add_indicator(hunger_indicator)
-dimension1.add_indicator(nutrition_indicator)
-
-dimension2.add_indicator(debt_indicator)
-dimension2.add_indicator(reserves_indicator)
-
-d1 = dimension1.get_data(orient="long")
-d2 = dimension2.get_data(orient="wide")
-
-idx = Index()
-idx.add_dimension(dimension1)
-idx.add_dimension(dimension2)
-
-test = idx.get_data(orient="wide")
+# get the index data
+df_index = index.get_data(orient='long')
