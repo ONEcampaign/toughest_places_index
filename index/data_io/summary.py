882123b94a06fcc47ce9138afc94de6ac5de6be2
Black formatter <data@one.org>
2022-06-08 20:20:03 +0000

    Fix code style issues with Black

--- index/data_io/summary.py
+++ index/data_io/summary.py
@@ -8,46 +8,63 @@ from index import common
 # Missing data
 # ====================================================
 
+
 def __missing_prop(df: pd.DataFrame, target_col: str, grouping_col: str = None) -> dict:
     """returns percent of a column that is null, by specified groups"""
 
     if grouping_col is None:
-        return {'overall':(df[target_col].isna().sum()/len(df))*100}
+        return {"overall": (df[target_col].isna().sum() / len(df)) * 100}
     else:
-        return {group: (df.loc[df[grouping_col] == group, target_col].isna().sum()/len(df.loc[df[grouping_col] == group]))*100
-                for group in df[grouping_col].unique()}
+        return {
+            group: (
+                df.loc[df[grouping_col] == group, target_col].isna().sum()
+                / len(df.loc[df[grouping_col] == group])
+            )
+            * 100
+            for group in df[grouping_col].unique()
+        }
 
 
-def __missing_countries_subset(df: pd.DataFrame, target_col:str, subset_col:str, iso_col:str = 'iso_code'):
+def __missing_countries_subset(
+    df: pd.DataFrame, target_col: str, subset_col: str, iso_col: str = "iso_code"
+):
     """ """
 
-    return {subset: df.loc[(df[subset_col] == subset)&(df[target_col].isna()), iso_col].unique()
-            for subset in df[subset_col].unique()}
+    return {
+        subset: df.loc[
+            (df[subset_col] == subset) & (df[target_col].isna()), iso_col
+        ].unique()
+        for subset in df[subset_col].unique()
+    }
 
 
-
-
-def missing_countries(df: pd.DataFrame, target_col:str, iso_col: str = 'iso_code', by = None) -> dict:
+def missing_countries(
+    df: pd.DataFrame, target_col: str, iso_col: str = "iso_code", by=None
+) -> dict:
     """ """
     if by is None:
-        return {'overall': df.loc[df[target_col].isna(), iso_col].unique()}
+        return {"overall": df.loc[df[target_col].isna(), iso_col].unique()}
 
-    elif by == 'continent':
-        df['subset'] = coco.convert(df[iso_col], to='continent')
-        return __missing_countries_subset(df, target_col, subset_col='subset')
+    elif by == "continent":
+        df["subset"] = coco.convert(df[iso_col], to="continent")
+        return __missing_countries_subset(df, target_col, subset_col="subset")
 
-    elif by == 'region':
-        df['subset'] = coco.convert(df[iso_col], to='UNregion')
-        return __missing_countries_subset(df, target_col, subset_col='subset')
+    elif by == "region":
+        df["subset"] = coco.convert(df[iso_col], to="UNregion")
+        return __missing_countries_subset(df, target_col, subset_col="subset")
 
-    elif by == 'income_level':
-        return __missing_countries_subset(common.add_income_levels(df), target_col, subset_col='income_level')
+    elif by == "income_level":
+        return __missing_countries_subset(
+            common.add_income_levels(df), target_col, subset_col="income_level"
+        )
 
     else:
-        raise ValueError(f'{by}: invalid parameter')
+        raise ValueError(f"{by}: invalid parameter")
 
 
-def summarize_missing(df: pd.DataFrame, target_col:str,by: str = 'overall', iso_col: str = 'iso_code') -> dict:
+def summarize_missing(
+    df: pd.DataFrame, target_col: str, by: str = "overall", iso_col: str = "iso_code"
+) -> dict:
     """
     :param target_col:
     :param df:
@@ -57,31 +74,35 @@ def summarize_missing(df: pd.DataFrame, target_col:str,by: str = 'overall', iso_
     """
 
     match by:
-        case 'overall':
+        case "overall":
             return __missing_prop(df, target_col)
 
-        case 'region':
-            return __missing_prop(df.assign(group = lambda d: coco.convert(d[iso_col], to = 'UNregion')),
-                                  target_col,
-                                  'group'
-                                  )
-        case 'continent':
-            return __missing_prop(df.assign(group = lambda d: coco.convert(d[iso_col], to = 'continent')),
-                           target_col,
-                           'group'
-                           )
+        case "region":
+            return __missing_prop(
+                df.assign(group=lambda d: coco.convert(d[iso_col], to="UNregion")),
+                target_col,
+                "group",
+            )
+        case "continent":
+            return __missing_prop(
+                df.assign(group=lambda d: coco.convert(d[iso_col], to="continent")),
+                target_col,
+                "group",
+            )
 
-        case 'income_level':
-            return __missing_prop(common.add_income_levels(df), target_col, 'income_level')
+        case "income_level":
+            return __missing_prop(
+                common.add_income_levels(df), target_col, "income_level"
+            )
 
-        case 'country':
+        case "country":
             return __missing_prop(df, target_col, iso_col)
 
         case _:
-            raise ValueError(f'Invalid parameter: {by}')
+            raise ValueError(f"Invalid parameter: {by}")
 
 
-def summarize_missing_full(df:pd.DataFrame, index_col:str = None) -> dict:
+def summarize_missing_full(df: pd.DataFrame, index_col: str = None) -> dict:
     """calculate % missing across all columns in a dataframe
     keep - specify the category and the group to keep eg {'income_level': 'Low income'}
 
@@ -90,8 +111,9 @@ def summarize_missing_full(df:pd.DataFrame, index_col:str = None) -> dict:
     if index_col is not None:
         df = df.set_index(index_col)
 
-    return df.assign(missing = lambda d: (d.isna().sum(axis=1)/len(d.columns))*100)['missing'].to_dict()
-
+    return df.assign(missing=lambda d: (d.isna().sum(axis=1) / len(d.columns)) * 100)[
+        "missing"
+    ].to_dict()
 
 
 # ====================================================
@@ -99,11 +121,10 @@ def summarize_missing_full(df:pd.DataFrame, index_col:str = None) -> dict:
 # ====================================================
 
 
-
 def __3sigma_test(series: pd.Series) -> list:
     """Uses the imperical rule to determine if a value is an outlier, returns a boolean list"""
 
-    return abs(series - series.mean()) > 3*series.std()
+    return abs(series - series.mean()) > 3 * series.std()
 
 
 def __iqr_test(series: pd.Series, factor: float = 2) -> list:
@@ -115,30 +136,45 @@ def __iqr_test(series: pd.Series, factor: float = 2) -> list:
     q25, q75 = series.quantile(0.25), series.quantile(0.75)
     iqr = q75 - q25
 
-    return (series < (q25 - (iqr * factor)))|(series> (q75 + (iqr * factor)))
+    return (series < (q25 - (iqr * factor))) | (series > (q75 + (iqr * factor)))
 
-AVAILABLE_METHODS = {'empirical': __3sigma_test, 'inter_quartile_range': __iqr_test}
 
-def _get_outliers(df: pd.DataFrame, target_col: str, method: str = 'empirical', cluster_col: str = None):
+AVAILABLE_METHODS = {"empirical": __3sigma_test, "inter_quartile_range": __iqr_test}
+
+
+def _get_outliers(
+    df: pd.DataFrame,
+    target_col: str,
+    method: str = "empirical",
+    cluster_col: str = None,
+):
     """ """
 
     if method not in AVAILABLE_METHODS.keys():
-        raise ValueError(f'{method}: invalid method')
+        raise ValueError(f"{method}: invalid method")
     method_func = AVAILABLE_METHODS[method]
     if cluster_col is None:
-        df_outlier =  df[method_func(df[target_col])]
+        df_outlier = df[method_func(df[target_col])]
 
     else:
         df_outlier = pd.DataFrame()
         for cluster in df[cluster_col].unique():
             cluster_df = df[df[cluster_col] == cluster]
-            df_outlier = pd.concat([df_outlier, cluster_df[method_func(cluster_df[target_col])]], ignore_index=True)
+            df_outlier = pd.concat(
+                [df_outlier, cluster_df[method_func(cluster_df[target_col])]],
+                ignore_index=True,
+            )
 
     return df_outlier
 
 
-
-def outliers(df: pd.DataFrame, target_col:str, method: str = 'empirical', cluster: str = None, iso_col: str = 'iso_code'):
+def outliers(
+    df: pd.DataFrame,
+    target_col: str,
+    method: str = "empirical",
+    cluster: str = None,
+    iso_col: str = "iso_code",
+):
     """
 
     :param df:
@@ -153,27 +189,32 @@ def outliers(df: pd.DataFrame, target_col:str, method: str = 'empirical', cluste
         case None:
             return _get_outliers(df, target_col, method)
 
-        case 'region':
-            return _get_outliers(df.assign(cluster = lambda d: coco.convert(d[iso_col], to='UNregion')),
-                          target_col, method, cluster_col= 'cluster'
-                          )
+        case "region":
+            return _get_outliers(
+                df.assign(cluster=lambda d: coco.convert(d[iso_col], to="UNregion")),
+                target_col,
+                method,
+                cluster_col="cluster",
+            )
 
-        case 'continent':
-            return _get_outliers(df.assign(cluster = lambda d: coco.convert(d[iso_col], to='continent')),
-                                 target_col, method, cluster_col= 'cluster'
-                                 )
+        case "continent":
+            return _get_outliers(
+                df.assign(cluster=lambda d: coco.convert(d[iso_col], to="continent")),
+                target_col,
+                method,
+                cluster_col="cluster",
+            )
 
-        case 'income_group':
-            return _get_outliers(common.add_income_levels(df), target_col, method, cluster_col='income_level')
+        case "income_group":
+            return _get_outliers(
+                common.add_income_levels(df),
+                target_col,
+                method,
+                cluster_col="income_level",
+            )
 
-        case 'country':
+        case "country":
             return _get_outliers(df, target_col, method, cluster_col=iso_col)
 
         case _:
-            raise ValueError(f'Invalid parameter: {cluster}')
-
-
-
-
-
-
+            raise ValueError(f"Invalid parameter: {cluster}")
